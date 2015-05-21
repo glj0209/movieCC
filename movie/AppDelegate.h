729414c440cfd23be483eca0bace3da6cea5f554
gaolijun <834740147@qq.com>
2015-05-21 11:02:44 +0800

    Initial Commit

---
+++ movie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  movie
+//
+//  Created by gaolijun on 15/5/21.
+//  Copyright (c) 2015年 cn.toura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ movie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  movie
+//
+//  Created by gaolijun on 15/5/21.
+//  Copyright (c) 2015年 cn.toura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
